0d4a654b58313a1b42fc7e02312cd0be1af60145
Lupita Morcillo Zavala <152880@mail.unacar.mx>
2022-03-13 23:23:36 -0600

    logre que se me reflejara el resultado

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+/* var Celsius=prompt("Pon la cantidad para convertilos de Farenheit a Celsius")//pregunto al usuario 
+
+var Celsius= (variable-32)/1.80----> como hacer la convercion
+ */
+/* window.alert(5 + 6);*/ /* probando maneras de mostrar el resultado */
+
+/* window.alert((prompt-32 )/1.80); */ //lo consegui pero no me daba la cantidad
+var F=prompt ("Pon la cantidad para convertilos de Farenheit a Celsius");
+var C=(F-32)/1.8;
+alert ("La temperatua a grados Celsius es "+ C)
