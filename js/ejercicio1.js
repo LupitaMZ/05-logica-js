6d2e4266514c3b64499ca2cca67f80f738146687
Lupita Morcillo Zavala <152880@mail.unacar.mx>
2022-03-15 11:18:10 -0600

    estan su respectiva carpeta

---
+++ js/ejercicio1.js
@@ -0,0 +1,13 @@
+/* console.log(20*2) */ /* prueba*/
+/* prompt("¿Hiciste la tarea?") */
+    //aqui va el codigo a usar
+var preguntaTarea=prompt("¿Hiciste tu tarea? pon si o no")//pregunto al usuario 
+ 
+//la verificaicon
+if(preguntaTarea === 'si'){
+    alert('Bien hecho')
+}else if (preguntaTarea ==='no'){
+    alert ('No te rindas, empiza ya mismo')
+}else {
+    alert('Por favor escribe como se te pidio')
+}
---
+++ js/ejercicio3.js
@@ -0,0 +1,9 @@
+/* var num=prompt ("Dame 3 numeros: ")
+if (num>8){
+    alert("el numero es mayos")
+}else{
+    alert("no es mayor")
+} */
+function compararNum(num1,num2,num3){
+
+}
