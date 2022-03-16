44b849212816929d59bf709937f251d9427f297d
Lupita Morcillo Zavala <152880@mail.unacar.mx>
2022-03-15 21:13:49 -0600

    le puse un comentario

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -4,6 +4,13 @@ if (num>8){
 }else{
     alert("no es mayor")
 } */
-function compararNum(num1,num2,num3){
+/* function compararNum(num1,num2,num3){
+    num1=prompt("dame un numero")
+    num2=prompt("dame un numero")
+    num3=prompt("dame un numero")
+    var resultado= num1+num2+num3
 
+    return resultado
 }
+var resultadoTotal=compararNum
+alert("la suma es" + resultadoTotal) */
